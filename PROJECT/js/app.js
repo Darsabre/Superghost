ed1bb85b38f696794bf613bf645efd23f7018d37
Ben <Darsabre@gmail.com>
2015-10-15 09:48:31 -0400

    outline

---
+++ PROJECT/js/app.js
@@ -0,0 +1,15 @@
+turn=1
+
+
+window.onload{
+  document.getElementById("before").addEventListener("click", );
+  document.getElementById("after").addEventListener("click", );
+  document.getElementById("challenge").addEventListener("click", );
+}
+
+if(turn%2=0) {
+  currentPlayer = player2
+}
+else  {
+  currentPlayer = player1
+}
